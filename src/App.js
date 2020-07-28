4b29844d6d464321b0d1668e0975d4fbf8cd2cf3
TheAstutian <speakingatoms@gmail.com>
2020-07-28 13:05:43 +0100

    added route to crwn-clothing app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HomePage} />
+      </Switch>
+      
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,8 +4,7 @@ import MenuItem from '../menu-item/menu-item.component';
 
 class Directory extends React.Component {
     constructor(){
-        super();
-
+        super()
         this.state = {
             sections: [
                 {
@@ -43,13 +42,13 @@ class Directory extends React.Component {
             ]
         }
         
-    }
+    } 
 
     render () {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl ,id, size}) =>(
+                    this.state.sections.map(({title, imageUrl, id, size}) =>(
                         <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
                     ))
                 }  
