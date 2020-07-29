46f04c41f9594bc0df914099790d25969547964e
TheAstutian <speakingatoms@gmail.com>
2020-07-29 11:10:53 +0100

    added collection-items and styled it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 import './App.css';
 
 function App() {
@@ -8,7 +10,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
       
     </div>
