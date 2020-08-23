12fa779129532a6bea4927134e40d16c2c8171d0
TheAstutian <speakingatoms@gmail.com>
2020-08-23 09:28:55 +0100

    created collection-overview component, prep app for advanced rounting

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
 
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component.jsx';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
-
-import {selectCollections} from '../../redux/shop/shop.selector'; 
-
-const ShopPage = ({itemsss}) => (
+const ShopPage = () => (
     <div className="shop-page">
-                {
-                    itemsss.map(({id, ...otherCollectionProps})=>(
-                        <CollectionPreview key={id} {...otherCollectionProps}/>
-                    ))
-                }
+                <CollectionsOverview />
             </div>
 );
    
-const mapStateToProps = createStructuredSelector({
-    itemsss: selectCollections
-})
 
-export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
