7ab4915b509bcb4a12e853eda30252370e021467
TheAstutian <speakingatoms@gmail.com>
2020-11-20 10:18:29 +0100

    created collection preview styled component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.style.scss';
+import {CollectionPreviewStyle, Title, Preview} from './collection-preview.style';
 
 const CollectionPreview =( {title,items}) =>( 
 
-    <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <div className="preview">
+   <CollectionPreviewStyle>
+         <Title>{title.toUpperCase()}</Title>
+                <Preview>
 
-            {
-                items
-                .filter((item,idx)=> idx<4)
-                .map((item)=>(
-                    <CollectionItem key={item.id} item={item} />
-                )) 
-            }
+                    {
+                        items
+                        .filter((item,idx)=> idx<4)
+                        .map((item)=>(
+                            <CollectionItem key={item.id} item={item} />
+                        )) 
+                    }
 
-        </div>
-    </div>
+                </Preview>
+   </CollectionPreviewStyle>
 )
 
 
