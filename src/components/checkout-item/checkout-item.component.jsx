d5c22aa9ab3b93fa392fabf54165f4d11d8d55a4
TheAstutian <speakingatoms@gmail.com>
2020-11-21 11:24:46 +0100

    added styled components for checkout-item and collection-item comps

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -4,24 +4,25 @@ import {connect} from 'react-redux';
 import {clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
 
 import './checkout-item.style.scss'
+import {CheckoutItemS, ImageContainer, Image, Name, Quantity, Arrow, Value, Price, RemoveButton} from './checkout-item.style';
 
 const CheckoutItem = ({cartItem, clearItem, addItem, removeItem}) => {
     const {name,imageUrl,price,quantity} = cartItem;
 
-    return (<div className='checkout-item'>
-        <div className='image-container'>
-            <img src={imageUrl} alt='item' />
-        </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>
-            <div className='arrow' onClick={()=>removeItem(cartItem)}>&#10094;</div>
-          <span className='value'>{quantity}</span>  
-            <div className='arrow' onClick={()=>addItem(cartItem)}>&#10095;</div>
-            </span>
-        <span className='price'>{price}</span>
-        <div className='remove-button' onClick={()=>clearItem(cartItem)}> &#10005; </div>
-
-    </div>)
+    return (<CheckoutItemS>
+            <ImageContainer>
+                <Image src={imageUrl} alt='item' />
+            </ImageContainer>
+            <Name>{name}</Name>
+            <Quantity>
+                <Arrow onClick={()=>removeItem(cartItem)}> &#10094;</Arrow>
+                <Value>{quantity}</Value>
+                <Arrow onClick={()=>addItem(cartItem)}> &#10095;</Arrow>
+            </Quantity>
+            <Price> {price}</Price>
+            <RemoveButton onClick={()=>clearItem(cartItem)}> &#10005;</RemoveButton>
+        
+    </CheckoutItemS>)
 }
 
 const mapDispatchToProps = dispatch => ({
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,23 +5,19 @@ import {addItem} from '../../redux/cart/cart.actions';
 
 import './collection-item.style.scss';
 import CustomButton from '../custom-button/custom-button.component';
+import {CollectionItemS, Image, CollectionFooter, Name, Price, CustomButtonS} from './collection-item.style';
 
 const CollectionItem = ({item, addItem}) => {
     const {name, price, imageUrl} = item; 
     return (
-    <div className='collection-item'>
-        <div className='image'
-        style={{
-            backgroundImage: `url(${imageUrl})`
-        }}
-        />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
-            <CustomButton onClick={()=>addItem(item)} inverted>Add to cart</CustomButton>
-        
-    </div>
+    <CollectionItemS>
+            <Image className='image' imageUrl={imageUrl}/>
+        <CollectionFooter>
+            <Name>{name}</Name>
+            <Price>{price}</Price>
+        </CollectionFooter>
+            <CustomButtonS onClick={()=>addItem(item)} inverted>Add to cart</CustomButtonS>
+    </CollectionItemS>
 )};
 
 const mapDispatchToProps = dispatch => ({
