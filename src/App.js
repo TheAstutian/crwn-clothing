7cf7d968351545fe9a16510b6fc3d58dc2ca07ae
TheAstutian <speakingatoms@gmail.com>
2021-02-24 12:42:30 +0100

    setup firestore for moving database to backend

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import CheckoutPage from './pages/checkout/checkout-component'; 
 
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import {selectCurrentUser} from './redux/user/user.selectors';
+import {selectCollectionsForPreview} from './redux/shop/shop.selector';
 
 import './App.css';
 import ScrollToTop from './ScrollToTop';
@@ -24,7 +25,7 @@ class App extends React.Component {
 
   componentDidMount(){
 
-    const {setCurrentUser} = this.props; 
+    const {setCurrentUser, collectionsArray} = this.props; 
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       if (userAuth) {
@@ -40,6 +41,7 @@ class App extends React.Component {
         
       }
         setCurrentUser(userAuth);
+        addCollectionAndDocuments('collections', collectionsArray)
     })
   }
 
@@ -67,7 +69,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 })
 
 const mapDispatchToProps = dispatch => ({
